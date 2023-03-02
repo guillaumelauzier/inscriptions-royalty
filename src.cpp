551a21de217ac82366ed6fefb880d8e09677eafc
Guillaume Lauzier <55703540+guillaumelauzier@users.noreply.github.com>
2023-03-02 23:33:32 +0100

    Create src.cpp

---
+++ src.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bitcoinconsensus.h>
+#include <bitcoin/bitcoin.hpp>
+
+int main() {
+  // Connect to Bitcoin Core
+  bc::config::endpoint endpoint("127.0.0.1", "8332");
+  bc::client::obelisk_client client(endpoint);
+
+  // Set the recipient and amount for the royalty payment
+  std::string recipient = "1SomeBitcoinAddress";
+  int64_t amount = 1000000; // 0.01 BTC
+
+  // Create a new transaction
+  bc::transaction tx;
+  tx.set_version(1);
+
+  // Create the input for the transaction (use an existing unspent output)
+  bc::hash_digest tx_hash;
+  bc::decode_hash(tx_hash, "d6b67f3b4c6d4ad6f13c55609bf2667b2a93eab21f6d33b6fc4f3fc4d6fc9287");
+  uint32_t tx_index = 0;
+  bc::output_point input(tx_hash, tx_index);
+  tx.inputs().push_back(bc::input(input));
+
+  // Create the output for the royalty payment
+  bc::script royalty_script = bc::script(bc::script().to_pay_key_hash_pattern(bc::address(recipient)));
+  bc::data_chunk royalty_data(amount);
+  std::fill(royalty_data.begin(), royalty_data.end(), 0x00);
+  royalty_script.push_operation(bc::operation(bc::machine::opcode::op_return, royalty_data));
+  bc::transaction_output royalty_output(amount, royalty_script);
+  tx.outputs().push_back(royalty_output);
+
+  // Sign the transaction
+  bc::endorsement signature;
+  bc::script input_script = bc::script(bc::script().to_pay_key_hash_pattern(bc::address("1SomeAddress")));
+  bc::data_chunk pubkey_data;
+  bc::decode_base16(pubkey_data, "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef");
+  bc::ec_secret secret;
+  bc::decode_base16(secret, "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef");
+  bc::ec_point pubkey;
+  bc::secret_to_public(pubkey, secret);
+  bc::script().create_endorsement(signature, secret, input_script, tx, 0, bc::machine::sighash_algorithm::all);
+  bc::script().set_subscript(input_script, bc::script().to_pay_key_hash_pattern(bc::bitcoin_short_hash(pubkey)));
+  bc::script().push_operation(input_script, bc::operation(signature));
+  bc::script().push_operation(input_script, bc::operation(bc::to_chunk(pubkey)));
+  tx.inputs()[0].set_script(input_script);
+
+  // Validate the transaction
+  uint32_t consensus_flags = bitcoinconsensus_SCRIPT_VERIFY_P2SH;
+  int result = bitcoinconsensus_verify_script_with_amount(
+    bc::to_data_chunk(bc::serialize(tx.inputs()[0].script())),
+    bc::to_data_chunk(bc::serialize(royalty_script)),
+    amount,
+    consensus_flags
+  );
+  if (result != bitcoinconsensus_OK) {
+    std::cout << "Transaction validation failed with error code: " << result << std::endl;
+    return 1;
+  }
+  
+  // Broadcast the transaction
+  bool success = client.transaction_pool_broadcast(tx);
+  if (success) {
+    std::cout << "Transaction broadcast successful!" << std::endl;
+  } else {
+    std::cout << "Transaction broadcast failed!" << std::endl;
+  }
+
+  return 0;
